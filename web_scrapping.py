f9c78843cd790ca6e334439c76c6e9d3b8480810
Dmitry Bogatyr <diboan@yandex.ru>
2019-04-01 21:37:54 +0300

    Update with functions and args

--- web_scrapping.py
+++ web_scrapping.py
@@ -6,60 +6,59 @@ import matplotlib.pyplot as plt
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
 import re
-
-url="https://www.hubertiming.com/results/2018MLK"
-html=urlopen(url)
-
-soup = BeautifulSoup(html, features="html.parser")
-
-# Get title of the page
-#title = soup.title
-#print(title)
-#print(title.text)
-
-#Get all links on the page
-#links = soup.find_all('a', href=True)
-#for link in links:
-#    print(link['href'])
+import argparse
 
 
-# Get info in table
-data = []
-allrows = soup.find_all("tr")
-for row in allrows:
-    row_list = row.find_all("td")
-    dataRow = []
-    for cell in row_list:
-        dataRow.append(cell.text)
-    data.append(dataRow)
-data = data[4:]
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--url',
+                        '-u',
+                        default='https://www.hubertiming.com/results/2018MLK',
+                        help="Url to parse")
+
+    args = parser.parse_args()
+    url = args.url
+    html=urlopen(url)
+    soup = BeautifulSoup(html, features="html.parser")
+
+    data = gather_data(soup)
+    draw_graph(header_update(soup,data))
 
 
-df = pd.DataFrame(data)
-#print(df.head(2))
-#print(df.tail(2))
+def gather_data(soup):
+    # Get info in table
+    data = []
+    allrows = soup.find_all("tr")
+    for row in allrows:
+        row_list = row.find_all("td")
+        dataRow = []
+        for cell in row_list:
+            dataRow.append(cell.text)
+        data.append(dataRow)
+    data = data[4:]
+    df = pd.DataFrame(data)
+    return df
 
-header_list = []
-col_headers = soup.find_all('th')
-for col in col_headers:
-    header_list.append(col.text)
+def header_update(soup, df):
+    header_list = []
+    col_headers = soup.find_all('th')
+    for col in col_headers:
+        header_list.append(col.text)
+    df.columns = header_list
+    return df
 
-df.columns = header_list
-# first 2 lines
-#print(df.head(2))
+def draw_graph(updated_data):
+    df = updated_data.dropna(how='any')
 
-# get info about table
-#df.info()
-#df.shape
+    df['ChipTime_Minutes'] = pd.to_timedelta(df['Chip Time'])
+    df['ChipTime_Minutes'] = df['ChipTime_Minutes'].astype('timedelta64[s]') / 60
 
-df = df.dropna(how='any')
+    # Create graph compare gender
+    plt.bar(df['Gender'], df['ChipTime_Minutes'])
+    plt.xlabel('Gender')
+    plt.ylabel('ChipTime_Minutes')
+    plt.title("Cimparison of average minutes run by male and female")
+    plt.show()
 
-df['ChipTime_minutes'] = pd.to_timedelta(df['Chip Time'])
-df['ChipTime_minutes'] = df['ChipTime_minutes'].astype('timedelta64[s]') / 60
-#print(df[['Gender', 'ChipTime_minutes']].head())
-
-# Create graph
-plt.bar(df['Gender'],df['ChipTime_Minutes'])
-#plt.xlabel('Gender')
-#plt.ylabel('ChipTime_Minutes')
-#plt.title("Cimparison of average minutes run by male and female")
+if __name__ == '__main__':
+    main()
